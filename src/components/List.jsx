afedbc29cbc32d5356e1957a492d6302b9d04ad2
Vasil Todorovski <vasil.todorovski@symphony-solutions.eu>
2020-02-12 17:14:11 +0100

    Added sorters, filters and markers

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -12,18 +12,24 @@ class List extends Component {
                 gender: Math.floor(Math.random() * 2) ? "M" : "F"
             });
         }
-        const genres = ["fiction", "drama", "poetry"];
+        const genres = ["fiction", "drama", "poetry", "finance", "horor"];
         const list = [];
         for(let i=1000000; i>0; i--) {
+            let publishedDate = new Date((Math.floor(Math.random() * 1020) + 1000) +
+                "-" + ((Math.floor(Math.random() * 12) + 1)) + 
+                "-" + ((Math.floor(Math.random() * 28) + 1))
+                );
+            const author = authors[Math.floor(Math.random() * authors.length)];
+            const genre = genres[Math.floor(Math.random() * genres.length)];
+            if(i % 5 === 4 && genre === "horor") {
+                publishedDate = new Date((Math.floor(Math.random() * 1020) + 1000) + "-10-31");
+            }
             list.push({
                 isbn: Math.random() * 10000000000,
                 name: "Book about " + Math.floor(Math.random() * 1000000),
-                author: authors[Math.floor(Math.random() * authors.length)],
-                genre: genres[Math.floor(Math.random() * genres.length)],
-                publishedDate: new Date((Math.floor(Math.random() * 1020) + 1000) +
-                    "-" + ((Math.floor(Math.random() * 12) + 1)) + 
-                    "-" + ((Math.floor(Math.random() * 28) + 1))
-                    )
+                author,
+                genre,
+                publishedDate
             })
         }
 
@@ -31,8 +37,12 @@ class List extends Component {
 
         this.state = {
             list,
+            genres,
             startIndex,
             pageSize,
+            genreFilter: null,
+            genderFilter: null,
+            availableList: [...list],
             visibleList: list.filter((item, index) => index > startIndex && index < (startIndex + pageSize))
         };
 
@@ -45,27 +55,46 @@ class List extends Component {
 
     componentDidUpdate() {
         this.handleScrollEvents = true;
-        // this.handleScroll();
     }
 
     render() {
         return (
             <React.Fragment>
-                <div className="toolbar">
-                    <button>order by...</button>
+                <div>
+                    <button onClick={this.orderByBookName}>order by book name</button>
+                    <button onClick={this.orderByAuthorName}>order by author name</button>
+                    <input type="radio" name="genres" onClick={this.genreChange} value="all" />all genres
+                    {this.state.genres.map(genre => (<span key={genre}>
+                        <input type="radio" name="genres" onClick={this.genreChange} value={genre}/>{genre}
+                        </span>)
+                    )}
+                    <input type="radio" name="gender" onClick={this.genderChange} value="all" />all genders
+                    <input type="radio" name="gender" onClick={this.genderChange} value="M" />male authors
+                    <input type="radio" name="gender" onClick={this.genderChange} value="F" />female authors
                 </div>
-                <div className="scrolled-content-wrapper" onScroll={this.handleScroll}>
-                    <ul className="list">
+                <div onScroll={this.handleScroll}>
+                    <ul>
                         {
                             this.state.visibleList
-                                // .filter((item, index) => index < 1000)
-                                .map(item => (
-                                    <li key={item.isbn}>
-                                        {item.name + " by " + item.author.name
-                                            + ", genre: " + item.genre
-                                            + ", published on: " + item.publishedDate}
-                                    </li>
-                                ))
+                                .map(item => {
+                                    let classes = "";
+                                    if(item.genre === "horor" && item.publishedDate.getUTCMonth() === 9 && item.publishedDate.getUTCDate() === 31) {
+                                        classes += "halloween";
+                                    } else if(item.genre === "finance" && item.publishedDate.getUTCDay() === 5) {
+                                        let nextFriday = new Date(item.publishedDate.getTime());
+                                        nextFriday.setDate(nextFriday.getDate() + 7);
+                                        if(nextFriday.getMonth() === item.publishedDate.getMonth()) {
+                                            classes += "business";
+                                        }
+                                    }
+                                    return (
+                                        <li key={item.isbn} className={classes}>
+                                            {item.name + " by " + item.author.name
+                                                + ", genre: " + item.genre
+                                                + ", published on: " + item.publishedDate}
+                                        </li>
+                                    );
+                                })
                         }
                     </ul>
                 </div>
@@ -77,14 +106,15 @@ class List extends Component {
         if(this.state.startIndex + this.state.pageSize < this.state.list.length) {
             const newState = {...this.state};
             newState.list = [...this.state.list];
+            newState.availableList = [...this.state.availableList];
             newState.startIndex += 100;
-            newState.visibleList = newState.list.filter((item, index) => {
+            newState.visibleList = newState.availableList.filter((item, index) => {
                 return (index > newState.startIndex 
                     && index < (newState.startIndex + newState.pageSize));
             })
 
             this.setState(newState);
-            console.log(newState);
+            // console.log(newState);
         } else {
             this.handleScrollEvents = true;
         }
@@ -94,14 +124,15 @@ class List extends Component {
         if(this.state.startIndex > 0) {
             const newState = {...this.state};
             newState.list = [...this.state.list];
+            newState.availableList = [...this.state.availableList];
             newState.startIndex -= 100;
-            newState.visibleList = newState.list.filter((item, index) => {
+            newState.visibleList = newState.availableList.filter((item, index) => {
                 return (index > newState.startIndex 
                     && index < (newState.startIndex + newState.pageSize));
             })
 
             this.setState(newState);
-            console.log(newState);
+            // console.log(newState);
         } else {
             this.handleScrollEvents = true;
         }
@@ -109,8 +140,6 @@ class List extends Component {
 
     handleScroll = event => {
         if(this.handleScrollEvents) {
-            // console.log(event);
-
             var lastLi = document.querySelector("ul > li:last-child");
             var lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
             var pageOffset = window.pageYOffset + window.innerHeight;
@@ -122,6 +151,92 @@ class List extends Component {
                 this.loadUp();
             }
         }
+    }
+
+    orderByBookName = event => {
+        const newState = {...this.state};
+        newState.list = [...this.state.list];
+        newState.startIndex = 0;
+        newState.availableList = this.state.list.sort((a, b) => a.name > b.name ? 1 : -1);
+        if(newState.genreFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.genre === newState.genreFilter);
+        }
+        if(newState.genderFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.author.gender === newState.genderFilter);
+        }
+        newState.visibleList = newState.availableList.filter((item, index) => {
+            return (index > newState.startIndex 
+                && index < (newState.startIndex + newState.pageSize));
+        })
+
+        this.setState(newState);
+    }
+
+    orderByAuthorName = event => {
+        const newState = {...this.state};
+        newState.list = [...this.state.list];
+        newState.startIndex = 0;
+        newState.availableList = this.state.list.sort((a, b) => a.author.name > b.author.name ? 1 : -1);
+        if(newState.genreFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.genre === newState.genreFilter);
+        }
+        if(newState.genderFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.author.gender === newState.genderFilter);
+        }
+        newState.visibleList = newState.availableList.filter((item, index) => {
+            return (index > newState.startIndex 
+                && index < (newState.startIndex + newState.pageSize));
+        })
+
+        this.setState(newState);
+    }
+
+    genreChange = event => {
+        const newState = {...this.state};
+        newState.list = [...this.state.list];
+        newState.startIndex = 0;
+        if(event.target.value === "all") {
+            newState.genreFilter = null;
+        } else {
+            newState.genreFilter = event.target.value;
+        }        
+        newState.availableList = this.state.list.sort((a, b) => a.author.name > b.author.name ? 1 : -1);
+        if(newState.genreFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.genre === newState.genreFilter);
+        }
+        if(newState.genderFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.author.gender === newState.genderFilter);
+        }
+        newState.visibleList = newState.availableList.filter((item, index) => {
+            return (index > newState.startIndex 
+                && index < (newState.startIndex + newState.pageSize));
+        });
+
+        this.setState(newState);
+    }
+
+    genderChange = event => {
+        const newState = {...this.state};
+        newState.list = [...this.state.list];
+        newState.startIndex = 0;
+        if(event.target.value === "all") {
+            newState.genderFilter = null;
+        } else {
+            newState.genderFilter = event.target.value;
+        }        
+        newState.availableList = this.state.list.sort((a, b) => a.author.name > b.author.name ? 1 : -1);
+        if(newState.genreFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.genre === newState.genreFilter);
+        }
+        if(newState.genderFilter !== null) {
+            newState.availableList = newState.availableList.filter((item, index) => item.author.gender === newState.genderFilter);
+        }
+        newState.visibleList = newState.availableList.filter((item, index) => {
+            return (index > newState.startIndex 
+                && index < (newState.startIndex + newState.pageSize));
+        });
+
+        this.setState(newState);
     }
 }
 
